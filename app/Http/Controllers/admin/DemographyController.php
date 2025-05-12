aa151bd372947391a95400089cb0a03d8c2ec76d
Awais <anjumwais296@gmail.com>
2025-05-12 02:00:04 -0700

    work for tabs

--- app/Http/Controllers/admin/DemographyController.php
+++ app/Http/Controllers/admin/DemographyController.php
@@ -8,9 +8,19 @@ use Illuminate\Http\Request;
 
 class DemographyController extends Controller
 {
-   public function index()
+   public function index(Request $request)
    {
-        $demography= Demography::paginate(10);
-        return view('admin.demography.demoindex',compact('demography'));
+        $demography = Demography::query()
+         ->when($request->filled('search'), function ($query) use ($request) {
+            return $query->search($request->search); // <-- return added here
+         })
+         ->when($request->has('type'), function ($query) use ($request) {
+            return $query->where('type', $request->type); // <-- return also safe here
+         })
+         ->paginate(10);
+
+        $type = isset($request->type)? $request->type :'all';
+
+        return view('admin.demography.demoindex',compact('demography','type'));
    }
 }
